0b2634b6cc4fc988999154713d274bb2584568fc
Cristian Medina <cris.medina.morocho@gmail.com>
2026-06-16 12:04:19 -0500

    Initial backend setup

---
+++ backend/src/test/java/com/forze/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forze.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
